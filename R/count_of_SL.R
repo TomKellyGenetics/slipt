e579253d794c50a4a2fca050e2dc8671ac6a3065
TomKellyGenetics <tomkellygenetics@gmail.com>
2016-04-05 13:09:39 +1200

    add options for TS_SL or Onco_SL to table and count functions, and coersion to strong if syn_lethal is incorrectly given

--- R/count_of_SL.R
+++ R/count_of_SL.R
@@ -5,27 +5,33 @@
 #' @param significance String. Significance condition for p-values to use: none, raw, or adjusted with a valid method for p.adjust() Defaults to fdr (false discovery rate / BH).
 #' @param threshold Numeric. Significance threshold (alpha, type I error rate) to cut-off (raw or adjusted) p-values. Defaults to 0.05.
 #' @param syn_lethal String. Stringency of synthetic lethal directional condition. Defaults to 'strong': symmetric replications. Other options are 'query' or 'candidate' or one-directional conditions (which gene is low in a low-high condition) or 'weak' for either one-directional condition.
+#' @param ts_sl Logical. Defaults to TRUE. Whether to test for synthetic lethality against low or high gene perturbations for tumour supressor gene inactivation (default) or oncogenic overactivation (alternative).
 #' @keywords synthetic lethal, gene interaction, significance, post-processing, subset, slice
 #' @export
 #' @examples
 #' count_of_SL()
 #'
 count_of_SL <-
-function(x, significance = "fdr", threshold = 0.05, syn_lethal = "strong"){
+function(x, significance = "fdr", threshold = 0.05, syn_lethal = "strong", ts_sl = TRUE){
   if(syn_lethal == "strong") syn_lethal_condition <- 2
   if(syn_lethal == "weak") syn_lethal_condition <- c("C", "Q", 2)
   if(syn_lethal == "query") syn_lethal_condition <- c("Q", 2)
   if(syn_lethal == "candidate") syn_lethal_condition <- c("C", 2)
+  if(!(syn_lethal %in% c("strong", "weak", "query", "candidate"))){
+    syn_lethal_condition <- 2
+    warning("please give syn_lethal as strong, weak, query, or candidate. Defaulting to strong: symmetric replication of gene pairs.")
+  }
+  sl_index <- ifelse(ts_sl, 11, 12)
   if(significance == "holm"){
-    sum((x[,11] %in% syn_lethal_condition) & (as.numeric(x[,16])<threshold)) #SL interactions & Holm Sig
+    sum((x[,sl_index] %in% syn_lethal_condition) & (as.numeric(x[,16])<threshold)) #SL interactions & Holm Sig
   } else if(significance == "fdr"){
-    sum((x[,11] %in% syn_lethal_condition) & (as.numeric(x[,17])<threshold)) #SL interactions & FDR Sig
+    sum((x[,sl_index] %in% syn_lethal_condition) & (as.numeric(x[,17])<threshold)) #SL interactions & FDR Sig
   } else if(significance == "raw"){
-    sum((x[,11] %in% syn_lethal_condition) & (as.numeric(x[,15])<threshold)) #SL interactions & raw Sig
+    sum((x[,sl_index] %in% syn_lethal_condition) & (as.numeric(x[,15])<threshold)) #SL interactions & raw Sig
   } else if(significance == "none" | is.null(significance) | missing(significance) | is.na(significance)){
-    sum(x[,11] %in% syn_lethal_condition) #SL interactions without signficance
+    sum(x[,sl_index] %in% syn_lethal_condition) #SL interactions without signficance
   } else if(significance %in% p.adjust.methods){
-    sum((x[,11] %in% syn_lethal_condition) & (p.adjust(as.numeric(x[,15]), method = significance)<threshold))#SL interactions & custom Sig adjust
+    sum((x[,sl_index] %in% syn_lethal_condition) & (p.adjust(as.numeric(x[,15]), method = significance)<threshold))#SL interactions & custom Sig adjust
   } else {
     warning("Please give a valid significance condition: none, raw, or a valid method for p.adjust()")
   }
