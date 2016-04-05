8b94259a2bbe4841831e84171daabbc0e2121de5
TomKellyGenetics <tomkellygenetics@gmail.com>
2016-04-05 12:45:46 +1200

    add Symmetric SL condition on observed/expected low-high and high-low expressed gene pairs, reversed test also performed giving TS_SL and Onco_SL for low and high gene perturbations respectively

--- R/detect_SL.R
+++ R/detect_SL.R
@@ -13,24 +13,35 @@ function(query, datasetx){
     if(query %in% colnames(datasetx)){
       query.datax<-datasetx[,match(query, colnames(datasetx))]
 
-      aa<-apply(datasetx,2, function(x) chisq.test(table(query.datax, x)))
+      a<-apply(datasetx,2, function(x) chisq.test(table(query.datax, x)))
       chi.pv<-lapply(aa, function(x) x$p.value)
+      chi.df<-lapply(aa, function(x) x$parameter)
+      chi.sq<-lapply(aa, function(x) x$statistic)
       ee<-lapply(aa, function(x) x$expected)
       oo<-lapply(aa, function(x) x$observed)
-      synleth<-lapply(aa, function(x) ifelse(x$observed[1,1]<x$expected[1,1] & x$observed[1,3]>x$expected[1,3],1,0))
+      synlethTS<-lapply(aa, function(x) ifelse(x$observed[1,1]<x$expected[1,1] & x$observed[1,3]>x$expected[1,3] & x$observed[3,1]>x$expected[3,1],2,ifelse(x$observed[1,1]<x$expected[1,1] & x$observed[1,3]>x$expected[1,3],"Q",ifelse(x$observed[1,1]<x$expected[1,1] & x$observed[3,1]>x$expected[3,1],"C",0))))
+      synlethONCO<-lapply(aa, function(x) ifelse(x$observed[3,3]>x$expected[3,3] & x$observed[1,3]<x$expected[1,3] & x$observed[3,1]<x$expected[3,1],2,ifelse(x$observed[3,3]>x$expected[3,3] & x$observed[3,1]<x$expected[3,1],"Q",ifelse(x$observed[3,3]>x$expected[3,3] & x$observed[1,3]<x$expected[1,3],"C",0))))
       #Format Data for Output in CSV
       kp<-cbind(names(chi.pv),
-                colnames(datasetx)[match(names(chi.pv),colnames(datasetx))],
+                rownames(Data_Matrix_BRCA_RNASeq_Voom)[match(names(chi.pv),rownames(Data_Matrix_BRCA_RNASeq_Voom))],
                 unlist(lapply(oo,function(x) x[1,1])),
                 unlist(lapply(ee,function(x) x[1,1])),
                 unlist(lapply(oo,function(x) x[1,3])),
                 unlist(lapply(ee,function(x) x[1,3])),
-                unlist(synleth),unlist(chi.pv),
-                unlist(p.adjust(chi.pv)),
-                unlist(p.adjust(chi.pv, method="BH"))
+                unlist(lapply(oo,function(x) x[3,1])),
+                unlist(lapply(ee,function(x) x[3,1])),
+                unlist(lapply(oo,function(x) x[3,3])),
+                unlist(lapply(ee,function(x) x[3,3])),
+                unlist(synlethTS),
+                unlist(synlethONCO),
+                unlist(chi.sq),unlist(chi.df),
+                signif(unlist(chi.pv), 5),
+                signif(unlist(p.adjust(chi.pv)),5),
+                signif(unlist(p.adjust(chi.pv, method="BH")),5)
       )[order(unlist(chi.pv)),]
-      colnames(kp)<-c("Probe","Gene","ObsLow","ExpLow","ObsHigh",
-                      "ExpHigh","SynLethal","rawPval","adjPval_holm", "adjPval_FDR")
+      colnames(kp)<-c("Probe","Gene","ObsQLowCLow","ExpQLowCLow","ObsQLowCHigh","ExpQLowCHigh",
+                      "ObsQHighCLow","ExpQHighCLow","ObsQHighCHigh","ExpQHighCHigh",
+                      "TS_SynLethal","Onco_SynLethal","Chi-squared","df","rawPval","adjPval_holm", "adjPval_FDR")
       return(kp)
     }
     else return(list(head(rownames(dataset), n=nrow(dataset)), "Query gene not contained in the dataset. Please enter the gene symbol in UPPER CASE, examples are shown below. Please contact the curator, Tom Kelly, for more information: kelsi602@student.otago.ac.nz."))
