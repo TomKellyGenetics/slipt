69ecb9dbf8b2ca090d44c1d2aa3352802c3f2509
TomKellyGenetics <tomkellygenetics@gmail.com>
2016-04-05 15:02:48 +1200

    added export_SL_latex function and docs

---
+++ R/export_SL_latex.R
@@ -0,0 +1,11 @@
+#' An Export Function to process SLIPT predictions
+#'
+#' Exports the key synthetic lethal gene information in a LaTeX table. Can be done for all genes (output of \code{\link[slipt]{detect_SL}}) or with synthetic lethal conditions and significance thresholds met (output of \code{\link[slipt]{table_of_SL}}).
+#' @param x Data matrix. Synthetic Lethal predictions to process, typically the output of \code{\link[slipt]{detect_SL}} or \code{\link[slipt]{table_of_SL}}.
+#' @keywords synthetic lethal, gene interaction, significance, post-processing, subset, slice, LaTeX, tex, table
+#' @export
+#' @examples
+#' export_SL_latex()
+#'
+export_SL_latex <-
+function(x) print(xtable(head(x[, c(3, 4, 13, 15, 17)], 25), display=c("s", "d", "g", "g", "g", "g")), type="latex", file=paste0(deparse(substitute(x)), ".tex"))
