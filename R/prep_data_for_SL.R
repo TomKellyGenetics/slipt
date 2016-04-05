c813fe99ea6f18fea295be447824a343f775c606
TomKellyGenetics <tomkellygenetics@gmail.com>
2016-04-05 12:12:58 +1200

    annotated prep_data for roxygen docs

--- R/prep_data_for_SL.R
+++ R/prep_data_for_SL.R
@@ -1,3 +1,13 @@
+#' Prepare Dataset for SLIPT (Synthetic Lethal Predition)
+#'
+#' Prepare data (e.g., gene expression data) for synthetic lethal prediction (typically count or continous data) by bining samples into categorical data for each gene to be tested.
+#' @param dataset Dataset input to be formatted. Input is genes (rows) x samples (columns). Output data is transposed: samples (rows) x genes (columns).
+#' @param exclude Do you wish to exclude genes without quartile variation? Defaults to TRUE.
+#' @keywords quantile, categorical, binning, synthetic lethal
+#' @export
+#' @examples
+#' prep_data_for_SL()
+#'
 prep_data_for_SL <-
 function(dataset, exclude = T){
   #bin samples into low/med/high categories for expression of each gene
