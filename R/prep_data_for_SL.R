f660e8ceaa93077d8ba5fda05914b4a21127766d
TomKellyGenetics <tom.lost@gmail.com>
2016-07-26 01:19:36 +1200

    add names to datasetx, add n-quantiles to prep

--- R/prep_data_for_SL.R
+++ R/prep_data_for_SL.R
@@ -10,9 +10,9 @@
 #' prep_data_for_SL()
 #'
 prep_data_for_SL <-
-function(dataset, n = 3, exclude = T){
+function(dataset, n = 3L, exclude = T){
   #define the number of bins required
-  if(is.numeric(n)){
+  if(is.numeric(n) && !(is.integer(n))){
     n <- as.integer(round(n))
     warning("rounding n to nearest integer")
   }
@@ -20,18 +20,22 @@ function(dataset, n = 3, exclude = T){
     n <- n[1]
     warning("n must be an integer, taking first value")
   }
-  if(!(is.integer(n)){
+  if(!(is.integer(n))){
     n <- 3
     warning("n must be an integer")
   }
-  if(n > 1){
+  if(n < 1){
     n <- 3
     warning("n must be at least 2")
   }
   #bin samples into low/med/high categories for expression of each gene
   datasetx<-apply(dataset,1,function(y) ifelse(y<=quantile(as.numeric(y),1/n, na.rm=T),0,ifelse(y>quantile(as.numeric(y),(n-1)/n, na.rm=T),2,1)))
+  rownames(datasetx) <- colnames(dataset)
+  colnames(datasetx) <- rownames(dataset)
   if(exclude){
     #Exclude genes with Q1=Q2
     datasetx<-datasetx[,!(apply(dataset, 1, function(y) quantile(as.numeric(y),1/n, na.rm=T))==apply(dataset, 1, function(y) quantile(as.numeric(y),(n-1)/n, na.rm=T)))]
   }
+
 }
+
