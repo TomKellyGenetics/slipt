674446735dfed2b932dcab74c24ebfe624c6e817
TomKellyGenetics <tomkellygenetics@gmail.com>
2016-12-01 09:21:00 +1300

    enable data prep slipt-3

--- R/prep_data_for_SL.R
+++ R/prep_data_for_SL.R
@@ -30,7 +30,7 @@ function(dataset, n = 3L, exclude = T){
   }
   #bin samples into low/med/high categories for expression of each gene
   if(n == 2) datasetx<-apply(dataset,1,function(y) ifelse(y<=quantile(as.numeric(y),1/n, na.rm=T),0,1)) 
-  if(n > 3) datasetx<-apply(dataset,1,function(y) ifelse(y<=quantile(as.numeric(y),1/n, na.rm=T),0,ifelse(y>quantile(as.numeric(y),(n-1)/n, na.rm=T),2,1)))
+  if(n >= 3) datasetx<-apply(dataset,1,function(y) ifelse(y<=quantile(as.numeric(y),1/n, na.rm=T),0,ifelse(y>quantile(as.numeric(y),(n-1)/n, na.rm=T),2,1)))
   rownames(datasetx) <- colnames(dataset)
   colnames(datasetx) <- rownames(dataset)
   if(exclude & n > 2){
